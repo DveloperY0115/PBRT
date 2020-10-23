d4af7475e30da2dbdcf1eec6bea02f9acd4fc227
Yoo SeungWoo <dreamy1534@gmail.com>
2020-10-24 01:14:12 +0900

    feat(master): Implement Vector classes

---
+++ Sources/core/geometry.h
@@ -0,0 +1,199 @@
+//
+// Created by 유승우 on 2020/10/23.
+//
+
+#if defined(_MSC_VER)
+#define NOMINMAX
+#pragma once
+#endif
+
+#ifndef PBRT_CORE_GEOMETRY_H
+#define PBRT_CORE_GEOMETRY_H
+
+#include "pbrt.h"
+#include <iterator>
+#include <assert.h>
+
+namespace pbrt {
+
+    template <typename T>
+    class Vector2 {
+    public:
+        T x, y;
+
+        /*
+         * Default Constructor
+         * Create a Vector2 instance and
+         * initialize all elements to 0.
+         */
+        Vector2() { x = 0; y = 0; }
+
+        /*
+         * Constructor with initial values
+         */
+        Vector2(T x, T y) : x(x), y(y) {
+            assert(!HasNaNs());
+        }
+
+        bool HasNaNs() const {
+            return isnan(x) || isnan(y);
+        }
+
+        T operator[](int i) const {
+            assert(i >= 0 && i <= 1);
+            if (i == 0) return x;
+            return y;
+        }
+
+        T& operator[](int i) {
+            assert(i >= 0 && i <= 1);
+            if (i == 0) return x;
+            return y;
+        }
+
+        Vector2<T> operator+(const Vector2<T> &v) const {
+            return Vector2(x + v.x, y + v.y);
+        }
+
+        Vector2<T>& operator+(const Vector2<T> &v) {
+            x += v.x;
+            y += v.y;
+            return *this;
+        }
+
+        Vector2<T> operator-(const Vector2<T> &v) const {
+            return Vector2(x - v.x, y - v.y);
+        }
+
+        Vector2<T>& operator-(const Vector2<T> &v) {
+            x -= v.x;
+            y -= v.y;
+            return *this;
+        }
+
+        Vector2<T> operator*(T s) const {
+            return Vector2<T>(s*x, s*y);
+        }
+
+        Vector2<T>& operator*=(T s) {
+            x *= s;
+            y *= s;
+            return *this;
+        }
+    };
+
+    typedef Vector2<float> Vector2f;
+    typedef Vector2<int> Vector2i;
+
+    template <typename T>
+    class Vector3 {
+    public:
+        T x, y, z;
+
+        /*
+         * Default Constructor
+         * Create a Vector3 instance and
+         * initialize all element to 0.
+         */
+        Vector3() { x = 0; y = 0; z = 0; }
+
+        /*
+         * Constructor with initial values
+         */
+        Vector3(T x, T y, T z) : x(x), y(y), z(z) {
+            assert(!HasNaNs());
+        }
+
+        bool HasNaNs() const {
+            return isnan(x) || isnan(y) || isnan(z);
+        }
+
+        T operator[](int i) const {
+            assert(i >= 0 && i <= 2);
+            if (i == 0) return x;
+            if (i == 1) return y;
+            return z;
+        }
+
+        T& operator[](int i) {
+            assert(i >= 0 && i <= 2);
+            if (i == 0) return x;
+            if (i == 1) return y;
+            return z;
+        }
+
+        Vector3<T> operator+(const Vector3<T> &v) const {
+            return Vector3(x + v.x, y + v.y + z + v.z);
+        }
+
+        Vector3<T>& operator+(const Vector3<T> &v) {
+            x += v.x;
+            y += v.y;
+            z += v.z;
+            return *this;
+        }
+
+        Vector3<T> operator-(const Vector3<T> &v) const {
+            return Vector3(x - v.x, y - v.y + z - v.z);
+        }
+
+        Vector3<T>& operator-(const Vector3<T> &v) {
+            x -= v.x;
+            y -= v.y;
+            z -= v.z;
+            return *this;
+        }
+
+        Vector3<T> operator*(T s) const {
+            return Vector3<T>(s*x, s*y, s*z);
+        }
+
+        Vector3<T>& operator*=(T s) {
+            x *= s;
+            y *= s;
+            z *= s;
+            return *this;
+        }
+
+        Vector3<T> operator/(T f) const {
+            assert(f != 0);
+            float inv = (float) 1 / f;
+            return Vector3<T>(x * inv, y * inv, z * inv);
+        }
+
+        Vector3<T>& operator/(T f) {
+            assert(f != 0);
+            float inv = (float) 1 / f;
+            x *= inv;
+            y *= inv;
+            z *= inv;
+            return *this;
+        }
+
+        Vector3<T> operator-() const {
+            return Vector3<T>(-x, -y, -z);
+        }
+
+    };
+
+    // Binary multiplication operator
+    template <typename T>
+    inline Vector3<T> operator*(T s, const Vector3<T> &v) {
+        return v * s;
+    }
+
+    template <typename T>
+    inline Vector3<T> Abs(const Vector3<T>& v) {
+        return Vector3<T>(abs(v.x), abs(v.y), abs(v.z));
+    }
+
+    template <typename T>
+    inline T Dot(const Vector3<T> &v1, const Vector3<T> &v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+    typedef Vector3<float> Vector3f;
+    typedef Vector3<int> Vector3i;
+
+}
+
+#endif
---
+++ Sources/core/pbrt.h
@@ -0,0 +1,8 @@
+//
+// Created by 유승우 on 2020/10/23.
+//
+
+#ifndef PBRT_PBRT_H
+#define PBRT_PBRT_H
+
+#endif //PBRT_PBRT_H

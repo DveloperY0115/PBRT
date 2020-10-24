3d55d76674d6b0bf378b927bc43413afe56d5a29
Yoo SeungWoo <dreamy1534@gmail.com>
2020-10-24 21:50:32 +0900

    feat(master): Add includes & declarations

--- Sources/core/pbrt.h
+++ Sources/core/pbrt.h
@@ -5,4 +5,27 @@
 #ifndef PBRT_PBRT_H
 #define PBRT_PBRT_H
 
+#include <algorithm>
+
+// core/pbrt.h*
+// Global Include Files
+#include <type_traits>
+#include <algorithm>
+#include <cinttypes>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+// Global Forward Declarations
+template <typename T>
+class Vector2;
+template <typename T>
+class Vector3;
+template <typename T>
+class Point2;
+template <typename T>
+class Point3;
 #endif //PBRT_PBRT_H

93a449164f3fe4bcbc69b8e2a05925c31c108531
Yoo SeungWoo <dreamy1534@gmail.com>
2020-10-26 01:15:33 +0900

    feat(master): Add Travis-CI

---
+++ Scripts/travis_build.sh
@@ -0,0 +1,15 @@
+set -e
+
+export TRAVIS_COMPILER=g++-7
+export CXX=g++-7
+export CXX_FOR_BUILD=g++-7
+export CC=gcc-7
+export CC_FOR_BUILD=gcc-7
+
+export NUM_JOBS=1
+
+mkdir build
+cd build
+cmake ..
+make
+bin/UnitTests
